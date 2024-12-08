2a8723f7c78a4942bc89b35c5cfe4b34a24f7cb4
StoichoManolov <badmonsterxd@gmail.com>
2024-12-08 17:35:04 +0200

    UI fix

--- My_Recipes/settings.py
+++ My_Recipes/settings.py
@@ -30,7 +30,6 @@ DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost').split(',')
 
-CSRF_TRUSTED_ORIGINS = config('CSRF_TRUSTED_ORIGINS',).split(',')
 
 
 # Application definition

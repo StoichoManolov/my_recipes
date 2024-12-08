bc5783b092f0541ea61bdfc34016629ff8c5014d
StoichoManolov <badmonsterxd@gmail.com>
2024-12-08 16:47:09 +0200

    fix: csrf token

--- My_Recipes/settings.py
+++ My_Recipes/settings.py
@@ -30,6 +30,8 @@ DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost').split(',')
 
+CSRF_TRUSTED_ORIGINS = config('CSRF_TRUSTED_ORIGINS',).split(',')
+
 
 # Application definition
 
